3f4b20d4e5ba715db1ef5d41962cfd16ed788e9b
slavc <v.inderiakin.uk@gmail.com>
2020-10-17 16:00:51 -0700

    gitignore update

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 This is a repo contains Bake-off project 1 made for 209AS Human-Computer Interaction course.
 \
 \
+T9 stands for Text on 9 keys
+\
 \
 Contributors:
 - Viacheslav Inderiakin
---
+++ T9.py
@@ -0,0 +1,11 @@
+# this file makes use of code presented in this article
+# https://towardsdatascience.com/implementing-a-trie-data-structure-in-python-in-less-than-100-lines-of-code-a877ea23c1a1
+import logging
+
+
+class T9Trie(object):
+    pass
+
+
+if __name__ == "__main__":
+    print("")
