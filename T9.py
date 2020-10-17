75a26db49238507811f3f8adfe242a1560cadf9d
slavc <v.inderiakin.uk@gmail.com>
2020-10-17 16:35:14 -0700

    T9 setup

--- T9.py
+++ T9.py
@@ -1,11 +1,36 @@
-# this file makes use of code presented in this article
+# this file makes use of code presented in this resources
 # https://towardsdatascience.com/implementing-a-trie-data-structure-in-python-in-less-than-100-lines-of-code-a877ea23c1a1
+# https://github.com/npezolano/Python-T9-implementation
 import logging
 
 
-class T9Trie(object):
-    pass
+mapping = {1: ["'"],
+           2: ["a", "A", "b", "B", "c", "C"],
+           3: ["d", "D", "e", "E", "f", "F"],
+           4: ["g", "G", "h", "H", "i", "I"],
+           5: ["j", "J", "k", "K", "l", "L"],
+           6: ["m", "M", "n", "N", "o", "O"],
+           7: ["p", "P", "q", "Q", "r", "R", "s", "S"],
+           8: ["t", "T", "u", "U", "v", "V"],
+           9: ["w", "W", "x", "X", "y", "Y", "z", "Z"]}
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = None
+
+
+class TrieNode(object):
+    def __init__(self, char: str):
+        self.symbol = char
+        self.children = []
+        # Is it the last character of the word.`
+        self.word_finished = False
+        # How many times this character appeared in the addition process
+        self.counter = 1
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='logs/example.log', level=logging.DEBUG)
+    # setup logging
+    logging.basicConfig(filename='logs/T9_execution.log', level=logging.DEBUG)
+    t9 = Trie()
